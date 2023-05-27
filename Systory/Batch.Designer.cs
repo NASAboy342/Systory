5dd796c76bc5ab00ca745a437959b936fe32e283
Pin Sopheaktra <sopheaktra_pin3421@outlook.com>
2023-05-27 16:54:17 +0700

    update

--- Systory/Batch.Designer.cs
+++ Systory/Batch.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             Bt_Close = new Button();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            Bt_NewBatch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // Bt_Close
@@ -41,22 +48,86 @@
             Bt_Close.UseVisualStyleBackColor = true;
             Bt_Close.Click += Bt_Close_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.Teal;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 100);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(198, 414);
+            dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = Color.Teal;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(216, 100);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(390, 414);
+            dataGridView2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Batch";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(216, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Major";
+            // 
+            // Bt_NewBatch
+            // 
+            Bt_NewBatch.Location = new Point(531, 65);
+            Bt_NewBatch.Name = "Bt_NewBatch";
+            Bt_NewBatch.Size = new Size(75, 23);
+            Bt_NewBatch.TabIndex = 5;
+            Bt_NewBatch.Text = "New Batch";
+            Bt_NewBatch.UseVisualStyleBackColor = true;
+            // 
             // Batch
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(0, 192, 192);
-            ClientSize = new Size(602, 487);
+            ClientSize = new Size(618, 526);
+            Controls.Add(Bt_NewBatch);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
             Controls.Add(Bt_Close);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Batch";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Batch";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button Bt_Close;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label1;
+        private Label label2;
+        private Button Bt_NewBatch;
     }
 }
--- Systory/Form1.cs
+++ Systory/Form1.cs
@@ -10,8 +10,9 @@ namespace Systory
         private void Bt_Batch_Click(object sender, EventArgs e)
         {
             Batch batch = new Batch();
-            batch.Location = Pn_Info.Location;
-            batch.ShowDialog();
+            batch.TopLevel = false;
+            Pn_Info.Controls.Add(batch);
+            batch.Show();
         }
     }
 }
